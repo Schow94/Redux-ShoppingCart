9045dc21ea62b1302454f0fcde26091f3d2fcf20
Schow94 <Schow94@gmail.com>
2019-10-07 08:04:05 -0700

    Updates

---
+++ precache-manifest.a77711e7e6448a597ebe4a9c39dad3ca.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d442ca13cfcb8260867535a8c40e52a8",
+    "url": "/Redux-ShoppingCart/index.html"
+  },
+  {
+    "revision": "8bbd3c013ab360277a55",
+    "url": "/Redux-ShoppingCart/static/js/2.1f7c0401.chunk.js"
+  },
+  {
+    "revision": "59463591009b6ba82dc0",
+    "url": "/Redux-ShoppingCart/static/js/main.bbe8059a.chunk.js"
+  },
+  {
+    "revision": "f9b17083510fb009216c",
+    "url": "/Redux-ShoppingCart/static/js/runtime-main.374787ab.js"
+  },
+  {
+    "revision": "ee5f202e42ac1463fd038f0362f5797f",
+    "url": "/Redux-ShoppingCart/static/media/image1.ee5f202e.jpeg"
+  },
+  {
+    "revision": "1206f77796eef6f2251bba79579bc199",
+    "url": "/Redux-ShoppingCart/static/media/image10.1206f777.jpeg"
+  },
+  {
+    "revision": "b0ed3f00c36178a13de2540e374d3ee0",
+    "url": "/Redux-ShoppingCart/static/media/image11.b0ed3f00.jpeg"
+  },
+  {
+    "revision": "bd9aa390436989d6541332511a4114a7",
+    "url": "/Redux-ShoppingCart/static/media/image12.bd9aa390.jpeg"
+  },
+  {
+    "revision": "70c37711b3ccc47961176f1c4e0c3136",
+    "url": "/Redux-ShoppingCart/static/media/image13.70c37711.jpeg"
+  },
+  {
+    "revision": "ac51ba3a715d814f018ca1fae1c5bdff",
+    "url": "/Redux-ShoppingCart/static/media/image2.ac51ba3a.jpeg"
+  },
+  {
+    "revision": "030220458aeed8e72e051f798c3b4653",
+    "url": "/Redux-ShoppingCart/static/media/image3.03022045.jpeg"
+  },
+  {
+    "revision": "82f4b46f3a608a97145267ff5f26d010",
+    "url": "/Redux-ShoppingCart/static/media/image4.82f4b46f.jpeg"
+  },
+  {
+    "revision": "0d937565105785f29e1599144db545f0",
+    "url": "/Redux-ShoppingCart/static/media/image5.0d937565.jpeg"
+  },
+  {
+    "revision": "f8ccf767125564b75ceb8c796ddd9dde",
+    "url": "/Redux-ShoppingCart/static/media/image6.f8ccf767.jpeg"
+  },
+  {
+    "revision": "f67e82526702d189aa9b407a55dcb4ef",
+    "url": "/Redux-ShoppingCart/static/media/image7.f67e8252.jpeg"
+  },
+  {
+    "revision": "916865cdb9349ea93875c80829a1cdba",
+    "url": "/Redux-ShoppingCart/static/media/image8.916865cd.jpeg"
+  },
+  {
+    "revision": "d95853dedac09a940c525bbf7e0cb315",
+    "url": "/Redux-ShoppingCart/static/media/image9.d95853de.jpeg"
+  }
+]);
